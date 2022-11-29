84304594debfd3634f77e633d7fe951566876309
RSC007 <rushichitte1998@gmail.com>
2022-11-29 17:24:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GameCard from './Components/GameCard'
+
+function App() {
+
+  return (
+    <div className="App">
+      <GameCard />
+    </div>
+  )
+}
+
+export default App
